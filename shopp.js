ef0899b1d99627d8d608a29c2c000466263015d4
Wurren <warrenhaskins@gmail.com>
2011-11-22 09:53:36 +0000

    Made smaller by 10%

--- shopp.js
+++ shopp.js
@@ -12,16 +12,16 @@
 
 	$.fn.shoppp = function(options) {
 
-		// Global options. All thats really needed here is the Email Address and the Currency Code
+		// Global options. All thats really needed here is the Email Address and the Currency Code. Currency Codes can be gotten from Paypal's site.
 
-		$.fn.shoppp.optionz = {
+		$.fn.shoppp.opt = {
 			"email" : "",
 			"currency" : "EUR"
 		};
 		
 		// Mesh in your settings into the global settings. Like a boss.	
 
-		var settings = $.extend({}, $.fn.shoppp.optionz, options);
+		var settings = $.extend({}, $.fn.shoppp.opt, options);
 
 		// return object for chaining
 
@@ -29,18 +29,16 @@
 
 			// Getting and setting from the <ul /> & then building the string.
 
-			var product = $(this);
-				parent = $(this).parent().parent(),
-				itemTitle = $('.itemTitle', parent).text(),
-				itemDesc = $('.itemDesc', parent).text(),
-				itemPrice = $('.itemPrice', parent).text().substr(1),
+			var product = $(this),
+				itemTitle = product.find(".itemTitle").text(),
+				itemDesc = product.find(".itemDesc").text(),
+				itemPrice = product.find(".itemPrice").text(),
 				url = "https://www.paypal.com/cgi-bin/webscr?cmd=_cart&upload=1&business=" + settings.email + "&currency_code=" + settings.currency,
 				build = "&item_name_1=" + itemTitle + "&item_number_1=1&quantity_1=1&amount_1=" + itemPrice;
 
-			// Building the link, and changing the url to link to Paypal.
+			// Bulding the link, and changing the url to link to Paypal.
 
-			product.attr("href", url + build);
-
+			product.find('.buynow').attr("href", url + build);
 
 			// That's it. Nice and Simple.
 
